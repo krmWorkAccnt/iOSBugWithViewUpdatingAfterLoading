281859992d10a57f0d188d2bf7a3627c43920459
rodsinclair <rod.sinclair@focus.net.nz>
2021-02-12 18:58:15 +1300

    Add project files.

---
+++ iOSBug/iOSBug/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using iOSBug.ViewModel;
+using Xamarin.Forms;
+
+namespace iOSBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            this.BindingContext = new MainViewModel();
+            InitializeComponent();
+        }
+    }
+}
---
+++ iOSBug/iOSBug/ViewModel/MainViewModel.cs
@@ -0,0 +1,76 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace iOSBug.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+        bool isVisible;
+        ObservableRangeCollection<TestObject> results;
+
+        public ObservableRangeCollection<TestObject> Results
+        {
+            get => results;
+            set => SetProperty(ref results, value);
+        }
+
+        public bool IsVisible
+        {
+            get => isVisible;
+            set => SetProperty(ref isVisible, value);
+        }
+
+        public MainViewModel()
+        {
+            IsVisible = false;
+            Task.Run(async () =>
+            {
+                await SearchAsync();
+            });
+
+        }
+
+        protected async Task SearchAsync()
+        {
+            Results = new ObservableRangeCollection<TestObject>(await GetData());
+            IsVisible = true;
+        }
+
+        private async Task<ObservableRangeCollection<TestObject>> GetData()
+        {
+
+            return await Task.Run(() =>
+            {
+                return new ObservableRangeCollection<TestObject>()
+                {
+                    {new TestObject(){ Name = "Test One" } },
+                    {new TestObject(){ Name = "Test One" } },
+                    {new TestObject(){ Name = "Test One" } },
+                    {new TestObject(){ Name = "Test One" } },
+                    {new TestObject(){ Name = "Test One" } },
+                    {new TestObject(){ Name = "Test One" } },
+                    {new TestObject(){ Name = "Test One" } },
+                    {new TestObject(){ Name = "Test One" } },
+                    {new TestObject(){ Name = "Test One" } },
+                    {new TestObject(){ Name = "Test One" } },
+                    {new TestObject(){ Name = "Test One" } },
+                    {new TestObject(){ Name = "Test One" } },
+                };
+            });
+
+        }
+
+    }
+
+    public class TestObject
+    {
+        string name;
+        public string Name { get; set; }
+    }
+}
